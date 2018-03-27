af4c8777e7c5237e75019662727ff1696adcdad3
LIGHT\alexa <lightcanadian@gmail.com>
2018-03-26 23:42:21 -0400

    Ep 134 Done -Get the player screen direction

--- Battle_tank/Source/Battle_tank/TankPlayerController.cpp
+++ Battle_tank/Source/Battle_tank/TankPlayerController.cpp
@@ -20,7 +20,7 @@ void ATankPlayerController::BeginPlay()
 void ATankPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	
+	AimTowardCrossaihair();
 	//UE_LOG(LogTemp, Warning, TEXT("time: %f"), DeltaTime);
 	//tick 
 		//super
@@ -33,7 +33,40 @@ void ATankPlayerController::AimTowardCrossaihair()
 	{
 		return;
 	}
-	//get world location if line trace through crosshaire
+	FVector HitLocation;
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		//get world location if line trace through crosshaire
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation) const
+{
+	//find the Crosshaire location in pixel coordinate
+	int32 ViewportSizeX, ViewportSizeY;
+	FVector CameraWorldDirection, ignore;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	FVector2D ReticulLocation(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+	FVector LookDirection;
+	if (GetLookDirection(ReticulLocation, LookDirection))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *CameraWorldDirection.ToString());
+	}
+	
+	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D Location,FVector &OutLookDirection) const
+{	
+	FVector ignore;
+	//"De-project"the screen location of the crosshair
+	return DeprojectScreenPositionToWorld(
+		Location.X,
+		Location.Y,
+		ignore,
+		OutLookDirection
+	);
 }
 
 ATank * ATankPlayerController::GetControlledTank() const
--- Battle_tank/Source/Battle_tank/TankPlayerController.h
+++ Battle_tank/Source/Battle_tank/TankPlayerController.h
@@ -27,8 +27,18 @@ public:
 	
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
-
+	
 private:
 	//Move the barrel of the tank toward the intersecting between the worl ind the crosshaire
 	void AimTowardCrossaihair();
+	//cast a rayscan in the world through the UI and if it hit something will output an FVecor location of were it hit
+	bool GetSightRayHitLocation(FVector&) const;
+
+	UPROPERTY(EditAnywhere)
+		float CrossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+		float CrossHairYLocation = 0.3333;
+
+	bool GetLookDirection(FVector2D ,FVector& ) const;
 };
